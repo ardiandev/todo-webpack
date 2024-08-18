567338bb597e1d2dd037efe75a7afbcf06aa2166
Ardian Pradana <ardianysp18@gmail.com>
2024-08-18 20:12:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo App Webpack
+
+This app is built using web component and webpack!
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+	mode: "development",
+	entry: path.resolve(__dirname, "./src/index.js"),
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "dist"),
+		},
+		open: true,
+		port: 3000,
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			filename: "index.html",
+			template: path.resolve(__dirname, "./src/template.html"),
+			title: "Todo App"
+		})
+	]
+}
